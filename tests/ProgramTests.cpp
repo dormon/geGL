c29058ca02521016b7b379b40026fe8805063e38
Tomáš Milet <amillhouse@seznam.cz>
2019-04-03 18:30:14 +0200

    Modified tests

--- tests/ProgramTests.cpp
+++ tests/ProgramTests.cpp
@@ -17,37 +17,151 @@ TEST_CASE("Program buffer interface"){
     #version 450
     layout(local_size_x=64)in;
   
-    layout(binding=3,std430)buffer Data0{uint data0[];};
-    layout(binding=5,std430)buffer Data1{uint data1[];};
-    layout(binding=7,std430)buffer Data2{uint data2[];};
+    layout(binding=3,std430)buffer Data0{uint  data0[];};
+    layout(binding=5,std430)buffer Data1{uvec2 data1[];};
+    layout(binding=6,std430)buffer Data2{uvec3 data2[];};
+    layout(binding=7,std430)buffer Data3{uvec4 data3[];};
+    layout(binding=8,std430)buffer Data4{uint data4a;uint data4b;};
   
     void main(){
       data0[gl_GlobalInvocationID.x] = 
-      data1[gl_GlobalInvocationID.x] +
-      data2[gl_GlobalInvocationID.x];
+      data1[gl_GlobalInvocationID.x].x +
+      data2[gl_GlobalInvocationID.x].x +
+      data3[gl_GlobalInvocationID.x].x +
+      data4a + data4b;
     }
     ).");
     auto prg = std::make_shared<ge::gl::Program>(cs);
     auto const id = prg->getId();
-    GLint bufferVariableActiveResources = 0;
-    GLint bufferVariableMaxNameLength   = 0;
-    ge::gl::glGetProgramInterfaceiv(id,GL_BUFFER_VARIABLE,GL_ACTIVE_RESOURCES,&bufferVariableActiveResources);
-    ge::gl::glGetProgramInterfaceiv(id,GL_BUFFER_VARIABLE,GL_MAX_NAME_LENGTH,&bufferVariableMaxNameLength);
-    std::cerr << bufferVariableActiveResources << std::endl;
-    std::cerr << bufferVariableMaxNameLength << std::endl;
 
-    std::vector<char>buf(bufferVariableMaxNameLength);
-    for(GLint i=0;i<bufferVariableActiveResources;++i){
-      glGetProgramResourceName(id,GL_BUFFER_VARIABLE,i,buf.size(),nullptr,buf.data());
-      std::cerr << buf.data() << std::endl;
-      GLenum props = GL_BUFFER_BINDING;
-      GLint propVal;
-      glGetProgramResourceiv(id,GL_SHADER_STORAGE_BLOCK,i,1,&props,1,nullptr,&propVal);
-      std::cerr << "binding: " << propVal << std::endl;
+    {
+      GLint activeResources = 0;
+      GLint maxNameLength   = 0;
+      glGetProgramInterfaceiv(id,GL_BUFFER_VARIABLE,GL_ACTIVE_RESOURCES,&activeResources);
+      glGetProgramInterfaceiv(id,GL_BUFFER_VARIABLE,GL_MAX_NAME_LENGTH,&maxNameLength);
+      std::cerr << "GL_BUFFER_VARIABLE" << std::endl;
+      std::cerr << "  GL_ACTIVE_RESOURCES: " << activeResources << std::endl;
+      std::cerr << "  GL_MAX_NAME_LENGTH : " << maxNameLength << std::endl;
+
+      std::vector<char>buf(maxNameLength);
+      for(GLint i=0;i<activeResources;++i){
+        glGetProgramResourceName(id,GL_BUFFER_VARIABLE,i,buf.size(),nullptr,buf.data());
+        std::cerr << buf.data() << std::endl;
+
+        {
+          GLenum const props[] = {
+            GL_BUFFER_BINDING      ,
+            GL_BUFFER_DATA_SIZE    ,
+            GL_NUM_ACTIVE_VARIABLES,
+            GL_ACTIVE_VARIABLES    ,
+          };
+          std::string const propNames[] = {
+            "GL_BUFFER_BINDING      ",
+            "GL_BUFFER_DATA_SIZE    ",
+            "GL_NUM_ACTIVE_VARIABLES",
+            "GL_ACTIVE_VARIABLES    ",
+          };
+          GLsizei const nprops = sizeof(props)/sizeof(props[0]);
+          GLint propVal[nprops];
+          glGetProgramResourceiv(id,GL_SHADER_STORAGE_BLOCK,i,nprops,props,nprops,nullptr,propVal);
+
+          for(size_t i=0;i<nprops;++i)
+            std::cerr << "  " << propNames[i] <<": " << propVal[i] << std::endl;
+
+        }
+
+        {
+          GLenum const props[] = {
+            GL_BLOCK_INDEX  ,
+            GL_ARRAY_STRIDE ,
+            GL_OFFSET       ,
+            GL_MATRIX_STRIDE,
+            GL_IS_ROW_MAJOR ,
+          };
+          std::string const propNames[] = {
+            "GL_BLOCK_INDEX  ",
+            "GL_ARRAY_STRIDE ",
+            "GL_OFFSET       ",
+            "GL_MATRIX_STRIDE",
+            "GL_IS_ROW_MAJOR ",
+          };
+          GLsizei const nprops = sizeof(props)/sizeof(props[0]);
+          GLint propVal[nprops];
+          glGetProgramResourceiv(id,GL_BUFFER_VARIABLE,i,nprops,props,nprops,nullptr,propVal);
+
+          for(size_t i=0;i<nprops;++i)
+            std::cerr << "  " << propNames[i] <<": " << propVal[i] << std::endl;
+        }
+      }
     }
+    std::cerr << "" << std::endl;
+    {
+      GLint activeResources;
+      GLint maxNameLength  ;
+      glGetProgramInterfaceiv(id,GL_SHADER_STORAGE_BLOCK,GL_ACTIVE_RESOURCES,&activeResources);
+      glGetProgramInterfaceiv(id,GL_SHADER_STORAGE_BLOCK,GL_MAX_NAME_LENGTH ,&maxNameLength  );
+      std::cerr << "GL_SHADER_STORAGE_BLOCK" << std::endl;
+      std::cerr << "  GL_ACTIVE_RESOURCES: " << activeResources << std::endl;
+      std::cerr << "  GL_MAX_NAME_LENGTH : " << maxNameLength << std::endl;
+
+      std::vector<char>buf(maxNameLength);
+      for(GLint i=0;i<activeResources;++i){
+        glGetProgramResourceName(id,GL_SHADER_STORAGE_BLOCK,i,buf.size(),nullptr,buf.data());
+        std::cerr << buf.data() << std::endl;
+
+        {
+          GLenum const props[] = {
+            GL_BUFFER_BINDING      ,
+            GL_BUFFER_DATA_SIZE    ,
+            GL_NUM_ACTIVE_VARIABLES,
+            GL_ACTIVE_VARIABLES    ,
+          };
+          std::string const propNames[] = {
+            "GL_BUFFER_BINDING      ",
+            "GL_BUFFER_DATA_SIZE    ",
+            "GL_NUM_ACTIVE_VARIABLES",
+            "GL_ACTIVE_VARIABLES    ",
+          };
+          GLsizei const nprops = sizeof(props)/sizeof(props[0]);
+          GLint propVal[nprops];
+          glGetProgramResourceiv(id,GL_SHADER_STORAGE_BLOCK,i,nprops,props,nprops,nullptr,propVal);
+
+          for(size_t i=0;i<nprops;++i)
+            std::cerr << "  " << propNames[i] <<": " << propVal[i] << std::endl;
+
+        }
+
+        {
+          GLenum const props[] = {
+            GL_BLOCK_INDEX  ,
+            GL_ARRAY_STRIDE ,
+            GL_OFFSET       ,
+            GL_MATRIX_STRIDE,
+            GL_IS_ROW_MAJOR ,
+          };
+          std::string const propNames[] = {
+            "GL_BLOCK_INDEX  ",
+            "GL_ARRAY_STRIDE ",
+            "GL_OFFSET       ",
+            "GL_MATRIX_STRIDE",
+            "GL_IS_ROW_MAJOR ",
+          };
+          GLsizei const nprops = sizeof(props)/sizeof(props[0]);
+          GLint propVal[nprops];
+          glGetProgramResourceiv(id,GL_BUFFER_VARIABLE,i,nprops,props,nprops,nullptr,propVal);
+
+          for(size_t i=0;i<nprops;++i)
+            std::cerr << "  " << propNames[i] <<": " << propVal[i] << std::endl;
+        }
+      }
+    }
+
+
+
     REQUIRE(prg->getBufferBinding("data0") == 3);
     REQUIRE(prg->getBufferBinding("data1") == 5);
-    REQUIRE(prg->getBufferBinding("data2") == 7);
+    REQUIRE(prg->getBufferBinding("data2") == 6);
+    REQUIRE(prg->getBufferBinding("data3") == 7);
   }
   win.endFrame();
 }
